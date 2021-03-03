0b5972f1624e9fef931c58ed310fbf16b918c201
vasavi <vasavi5431@gmail.com>
2021-03-03 13:54:08 +1100

    react-profile-card

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Profile from './Components/Profile/Profile'
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Welcome!..</h1>
+     <Profile/>
+    </div>
+  );
+}
+
+export default App;
